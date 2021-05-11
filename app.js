986b8462baf40e5b8a487716bd87dbbbc06772c8
ShanJiang1911 <johnson33399@gmail.com>
2021-05-11 16:55:09 -0700

    complete lab06 in the right format

--- app.js
+++ app.js
@@ -1,134 +1,111 @@
 'use strict';
 
+const hoursOfOperations = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'];
 
 const seattle = {
-    name: 'Seattle',
-    customerNumber: 0,
-    time: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', 'Total:'],
-    avgCookieSale: 6.3,
-    hourlyCookieSale: 0,
-    
-    // getHourlyCookieSale: function() {
-    //     this.hourlyCookieSale = getCustomerNumber * avgCookieSale;
-    //     console.log(this.hourlyCookieSale);
-    // }
-
-    getCustomerNumber: function() {
-        this.customerNumber = randomCustomer(23,65);
-    }
-        
+    cityName: 'Seattle',
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookiePerCustomer: 6.3,
+    randomNumCustomer: function() {
+        let randomNumber = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+        return randomNumber;
+    }        
 }
-
-
 
 
 const tokyo = {
-    name: 'Tokyo',
-    customerNumber: 0,
-    time: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', 'Total:'],
-    avgCookieSale: 1.2,
-    getCustomerNumber: function() {
-        this.customerNumber = randomCustomer(3,24);
+    cityName: 'Tokyo',
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgCookiePerCustomer: 1.2,
+    randomNumCustomer: function() {
+        let randomNumber = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+        return randomNumber;
     }
 }
-
-
 
 
 const dubai = {
-    name: 'Dubai',
-    customerNumber: 0,
-    time: ['6am: ', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', 'Total:'],
-    avgCookieSale: 3.7,
-    getCustomerNumber: function() {
-        this.customerNumber = randomCustomer(11,38);
+    cityName: 'Dubai',
+    minCustomer: 11,
+    maxCustomer: 38,
+    avgCookiePerCustomer: 3.7,
+    randomNumCustomer: function() {
+        let randomNumber = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+        return randomNumber;
     }
 }
-
-
 
 
 const paris = {
-    name: 'Paris',
-    customerNumber: 0,
-    time: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', 'Total:'],
-    avgCookieSale: 2.3,
-    getCustomerNumber: function() {
-        this.customerNumber = randomCustomer(20,38);
+    cityName: 'Paris',
+    minCustomer: 20,
+    maxCustomer: 38,
+    avgCookiePerCustomer: 2.3,
+    randomNumCustomer: function() {
+        let randomNumber = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+        return randomNumber;
     }
 }
-
 
 
 const lima = {
-    name: 'Lima',
-    customerNumber: 0,
-    time: ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:', 'Total:'],
-    avgCookieSale: 4.6,
-    getCustomerNumber: function() {
-        this.customerNumber = randomCustomer(2,16);
+    cityName: 'Lima',
+    minCustomer: 2,
+    maxCustomer: 16,
+    avgCookiePerCustomer: 4.6,
+    randomNumCustomer: function() {
+        let randomNumber = Math.floor(Math.random()*(this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+        return randomNumber;
     }
-        
 }
 
 
-
-function randomCustomer(min, max) {
-    return Math.floor(Math.random()*(max - min + 1) + min);
+const locationDivElem = document.getElementById('SalesReport');
+    
+function generateSalesArray(location) {
+    location.grandTotal = 0;
+    location.hourlySaleArray = [];
+    for (let i = 0; i < hoursOfOperations.length; i++) {
+        let customers = location.randomNumCustomer();
+        let cookiesSold = Math.floor(customers * location.avgCookiePerCustomer);
+        location.grandTotal += cookiesSold;
+        location.hourlySaleArray.push(cookiesSold)
+    }
 }
 
-
-seattle.getCustomerNumber();
-tokyo.getCustomerNumber();
-dubai.getCustomerNumber();
-paris.getCustomerNumber();
-lima.getCustomerNumber();
-
-
-const salesArray = [seattle, tokyo, dubai, paris, lima];
-
-const salesList = document.getElementById("salesList");
-
-const avgCookieSale = ['6.3', '1.2', '3.7', '2.3', '4.6'];
+generateSalesArray(seattle);
+console.log(seattle)
+generateSalesArray(tokyo);
+generateSalesArray(dubai);
+generateSalesArray(paris);
+generateSalesArray(lima);
 
 
+function storeLocationInfo(location) {
 
-for (let j = 0; j < salesArray.length; j++) {
-    
-    let currentStore = salesArray[j];
-    
-    const article = document.createElement('article');
-    salesList.appendChild(article);
-
-    
     const h2Elem = document.createElement('h2');
-    h2Elem.textContent = currentStore.name;
-    article.appendChild(h2Elem);
-
-    const pElem = document.createElement('p');
-    pElem.textContent = currentStore.customerNumber;
-    article.appendChild(pElem);
+    h2Elem.textContent = location.cityName;
+    locationDivElem.appendChild(h2Elem);
 
     const ulElem = document.createElement('ul');
-    article.appendChild(ulElem);
+    locationDivElem.appendChild(ulElem);
 
+    for(let i = 0; i < hoursOfOperations.length; i++) {
+        const liElem = document.createElement('li');
+        liElem.textContent = `${hoursOfOperations[i]}: ${location.hourlySaleArray[i]} cookies`;
+        ulElem.appendChild(liElem);
+    }
 
-
-
-        for (let i = 0; i < currentStore.time.length; i++) {
-            const liElem = document.createElement('li');
-            liElem.textContent = `${currentStore.time[i]} ${currentStore.customerNumber + ' cookies'}`;
-            ulElem.appendChild(liElem);
-        }
-
+    const totalLiElem = document.createElement('li');
+    totalLiElem.textContent = `Total: ${location.grandTotal} cookies.`;
+    ulElem.appendChild(totalLiElem);
 }
 
-
-
-
-
-
-
-
-
+storeLocationInfo(seattle);
+storeLocationInfo(tokyo);
+storeLocationInfo(dubai);
+storeLocationInfo(paris);
+storeLocationInfo(lima);
 
